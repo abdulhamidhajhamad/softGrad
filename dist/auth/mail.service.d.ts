f2c03e12fd1dd3b3f3868f71073e2d3b57b5e3b1
abdulhamidhajhamad <abdulhamidhajhamad@gmail.com>
2026-01-26 00:10:38 +0200

    Remove dist folder from Git tracking

--- dist/auth/mail.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class MailService {
-    private transporter;
-    constructor();
-    private verifyTransporter;
-    sendVerificationEmail(email: string, verificationCode: string): Promise<void>;
-    sendPasswordResetEmail(email: string, resetUrl: string): Promise<void>;
-}
